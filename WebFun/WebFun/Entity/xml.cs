91d000d71bcaa5fc36826b943830e4857545990a
AmaralOne <flaviofilho2908@gmail.com>
2019-10-29 16:03:41 -0300

    refatoracao

---
+++ WebFun/WebFun/Entity/xml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace WebFun.Entity
+{
+    [XmlRoot("xml")]
+    public class xml
+    {
+        public xml() { }
+        public xml(List<Produto> produto)
+        {
+            this.produto = produto;
+        }
+        public List<Produto> produto { get; set; }
+    }
+}
